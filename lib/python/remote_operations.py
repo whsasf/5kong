a5c860ad8e8cab7a011caf74fb31a02c10ef995b
whsasf <whsasf@126.com>
2018-07-04 15:50:56 +0800

    new

--- lib/python/remote_operations.py
+++ lib/python/remote_operations.py
@@ -5,6 +5,7 @@ def remote_operation(sshhost,username,passwd,cmds,\
     confirmobj = '',\
     confirmobjcount = 1,\
     sshport = 22,\
+    keyfile = '',\
     outlog ='sshout.log',\
     errorlog ='ssherror.log',\
     paramikologenable = 0 \
@@ -14,7 +15,8 @@ def remote_operation(sshhost,username,passwd,cmds,\
 
     import paramiko                   # third party libs needs for ssh authentication
     import basic_class                # using log part
-
+    import os
+    
     sshhost = sshhost                 # ssh destination hosts,can be IP or resolvable hostnames
     username = username               # account-name used to establish ssh connection
     passwd = passwd                   # account-password used to establish ssh connection
@@ -22,7 +24,13 @@ def remote_operation(sshhost,username,passwd,cmds,\
     confirmflag = confirmflag         # if need check the outcome to confirm operation success or failed,default 1
     confirmobj = confirmobj           # the target need to to compared or searched or confirmed,default empty
     confirmobjcount = confirmobjcount # the accurance of confirmobj,default 1
-    sshport = sshport                     # defaule ssh connection port ,22 by default
+    sshport = sshport                 # defaule ssh connection port ,22 by default
+    
+    if keyfile == '':                 # private key file
+        keyfile = os.environ['HOME']+'/.ssh/id_rsa'
+    else:
+        keyfile = keyfile
+                                 
     outlog = outlog                       # normal ssh log file
     errorlog = errorlog                   # error ssh log file
     paramikologenable = paramikologenable # by default, paramikologdisabled ,set to 1 to enable            
@@ -33,8 +41,28 @@ def remote_operation(sshhost,username,passwd,cmds,\
             
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname = sshhost, port = sshport, username = username, password = passwd)
-
+    if os.path.exists(keyfile):          # keyfile exist, will try to ssh withot password
+        basic_class.mylogger_record.debug('Establishing ssh connection withOUT password ...')
+        private_key = paramiko.RSAKey.from_private_key_file(keyfile)
+        try:
+            ssh.connect(hostname = sshhost, port = sshport, username = username, pkey = private_key)
+        except:
+            basic_class.mylogger_record.debug('Establishing ssh connection withOUT password failed! Will continue try with password ...')
+            basic_class.mylogger_record.debug('Establishing ssh connection with password ...')
+            try:
+                ssh.connect(hostname = sshhost, port = sshport, username = username, password = passwd)
+            except:
+                basic_class.mylogger_record.warning('Establishing ssh connection with password failed! Please check manually! ')               
+                exit (1)
+                            
+    else:                                # keyfile not exist, will try to ssh with password
+        basic_class.mylogger_record.debug('Establishing ssh connection with password ...')
+        try:
+            ssh.connect(hostname = sshhost, port = sshport, username = username, password = passwd)
+        except:
+            basic_class.mylogger_record.warning('Establishing ssh connection with password failed! Please check manually! ')               
+            exit (1)
+            
     stdin, stdout, stderr = ssh.exec_command(cmds)
     okout = stdout.read()
     errout = stderr.read()
